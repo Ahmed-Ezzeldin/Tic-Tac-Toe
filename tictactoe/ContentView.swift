324e50bd2d43a083f115b76a8a7422cfc53a031c
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-17 15:13:31 +0200

    Initial Commit

---
+++ tictactoe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tictactoe
+//
+//  Created by Cloud Secrets on 17/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Cloud Secrets on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
